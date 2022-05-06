55579cc2564e2ea7aa4bf0a3be96c34af444ced2
asha <qkrwltnn@naver.com>
2022-05-07 04:15:01 +0900

    upload3

---
+++ main/java/net/webplate/controller/ListController.java
@@ -0,0 +1,26 @@
+package net.webplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/list/*")
+public class ListController {
+
+	
+	@RequestMapping(value="menu_list")
+	public void menu_list() {
+		
+	}
+	
+	@RequestMapping(value="test")
+	public void test() {
+		
+	}
+	
+	@RequestMapping(value="top_list")
+	public void top_list() {
+		
+	}
+	
+}
